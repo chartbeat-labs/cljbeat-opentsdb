91a2fb9a1844cb20a93f7e7ae82b897437694f8d
Rick Mangi <rick@chartbeat.com>
2015-07-20 17:08:28 -0400

    trying to get into clojars

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject com.chartbeat.opentsdb "0.1.2-SNAPSHOT"
+(defproject com.chartbeat.opentsdb "0.1.2"
   :description "Simple lil' clojure library for OpenTSDB"
   :url "https://github.com/chartbeat-labs/cljbeat-opentsdb"
   :license {:name "BSD 3 Clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [tcp-server "0.1.0"]]
+  :deploy-repositories [["releases" :clojars]]
+
   :aot :all
   :profiles {:benchmark {:main com.chartbeat.opentsdb.example_usage}}
   :aliases {"benchmark" ["with-profile" "benchmark" "run"]}
