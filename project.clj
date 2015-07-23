f00364a20f158b62f2818c491b65f76c9d942aa0
Rick Mangi <rick@chartbeat.com>
2015-07-23 15:13:02 -0400

    clojure 1.7 seemed to be causing some compile issues with clj 1.6 projects

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.chartbeat.opentsdb "0.1.4"
+(defproject com.chartbeat.opentsdb "0.1.4-SNAPSHOT"
   :description "Simple lil' clojure library for OpenTSDB"
   :url "https://github.com/chartbeat-labs/cljbeat-opentsdb"
   :license {:name "BSD 3 Clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [tcp-server "0.1.0"]]
   :deploy-repositories [["releases" :clojars]]
   :signing {:gpg-key "F0903068"}
